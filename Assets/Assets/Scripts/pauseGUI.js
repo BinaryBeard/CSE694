99a686f61a72d42d995950e7e6f2b54c308a8e4b
Sam <sam.bantner@me.com>
2012-06-04 10:48:58 -0400

    Small Fixes

--- Assets/Assets/Scripts/pauseGUI.js
+++ Assets/Assets/Scripts/pauseGUI.js
@@ -1,21 +1,5 @@
 #pragma strict
 
-/* GravityEffect
-	
-Planets:
-Earth - 1.0
-Mercuty - 0.3
-Venus - 0.9
-Moon - 0.1
-Mars - 0.3
-Jupiter - 2.3
-Saturn - 1.0
-Uranus - 0.8
-Neptune - 1.1
-Sun - 27.0
-
-*/
-
 class Planet {
 
 	var gravityEffect : float = 0;
@@ -46,12 +30,14 @@ function Start () {
 	planetArray[0] = new Planet(1.0, "Earth", 4000);
 	planetArray[1] = new Planet(0.3, "Mercury", 4000);
 	planetArray[2] = new Planet(0.9, "Venus", 4000);
-	planetArray[3] = new Planet(0.1, "Moon", 4000);
+	planetArray[3] = new Planet(0.7, "Undiscovered", 4000);
 	planetArray[4] = new Planet(0.3, "Mars", 4000);
 	planetArray[5] = new Planet(2.3, "Jupiter", 4000);
 	planetArray[6] = new Planet(1.0, "Saturn", 4000);
 	planetArray[7] = new Planet(0.8, "Uranus", 4000);
 	planetArray[8] = new Planet(1.1, "Neptune", 4000);
+	planetArray[9] = new Planet(0.6, "Undiscovered", 4000);
+
 }
 
 function Update () {
@@ -73,7 +59,7 @@ function OnGUI() {
 	if (GUI.Button (Rect (Screen.width - Screen.width/4.5,Screen.height/2.5,Screen.width/5,Screen.width/5), "", nextStyle)) {
 
        	// Take gravity to next level
-       	if (currentPlanet < planetArray.Length - 2) {
+       	if (currentPlanet < planetArray.Length - 1) {
        		
        		currentPlanet++;
        	}
@@ -92,7 +78,7 @@ function OnGUI() {
        	}
     }
     
-	GUI.Label (Rect(Screen.width/2 - 300, Screen.height/2 - 100, 600, 200), planetArray[currentPlanet].planetName, planetStyle);
+	GUI.Label (Rect(Screen.width/2 - 400, Screen.height/2 - 100, 800, 200), planetArray[currentPlanet].planetName, planetStyle);
 	GUI.Label (Rect(0, 10, Screen.width, 300), planetArray[currentPlanet].gravityEffect.ToString() + "x\n Earth's gravity", gravityStyle);
 
 	GameObject.Find("Cannon_System").GetComponent(cannon).gravityEffect = planetArray[currentPlanet].gravityEffect;
